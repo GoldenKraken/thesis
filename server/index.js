d333457561ff7543149fbfff5bb6b443c0751172
Kaiser Wu <kaiserwu@Kaisers-MacBook-Pro.local>
2017-12-15 14:35:32 -0800

    Refactor db files with MongoDB and Mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const db = require('../database/index.js');
+//const AbandonedTotal = require('../database/AbandonedTotal.js');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
