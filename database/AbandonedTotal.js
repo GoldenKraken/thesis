74f6389ab128320b747831499fd176cc3af771cd
Kaiser Wu <kaiserwu@Kaisers-MacBook-Pro.local>
2017-12-16 16:54:31 -0800

    Generate 10mil data points through external function

--- database/AbandonedTotal.js
+++ database/AbandonedTotal.js
@@ -12,25 +12,4 @@ const abandonedTotalSchema = new mongoose.Schema({
 
 const AbandonedTotal = mongoose.model('AbandonedTotal', abandonedTotalSchema);
 
-var generateData = () => {
-  var result = [];
-
-  for (var i = 0; i < 100; i++) {
-    result.push({
-      viewInstanceId: Math.floor(Math.random() * 10000000),
-      videoId: Math.floor(Math.random() * 100000),
-      watchTimestamp: `2017-12-08 ${Math.floor(Math.random() * 24)}:${Math.floor(Math.random() * 60)}:${Math.floor(Math.random() * 60)}+00:00`,
-      dayFlag: Math.floor(Math.random() * 2),
-      yearWeek: '2017-49',
-      abandonFlag: Math.floor(Math.random() * 2)
-    });
-  }
-
-  return result;
-};
-
-AbandonedTotal.insertMany(generateData(), (err, data) => {
-  if (err) { console.error(err); }
-});
-
 module.exports = AbandonedTotal;
